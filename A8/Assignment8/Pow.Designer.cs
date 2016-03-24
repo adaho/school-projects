8510d3558f165600ec85af1b128e21a7246553f7
adaho <adaho@asu.edu>
2016-03-24 12:03:59 -0700

    Added files via upload

---
+++ A8/Assignment8/Pow.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Assignment8
+{
+    partial class Pow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pow));
+            this.targetPictureBox = new System.Windows.Forms.PictureBox();
+            this.bulletPictureBox = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.fireButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.powPrefixLabel = new System.Windows.Forms.Label();
+            this.powSuffixLabel = new System.Windows.Forms.Label();
+            this.targetTimer = new System.Windows.Forms.Timer(this.components);
+            this.bulletTimer = new System.Windows.Forms.Timer(this.components);
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.powTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.targetPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bulletPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // targetPictureBox
+            // 
+            this.targetPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.targetPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("targetPictureBox.Image")));
+            this.targetPictureBox.Location = new System.Drawing.Point(24, 34);
+            this.targetPictureBox.Name = "targetPictureBox";
+            this.targetPictureBox.Size = new System.Drawing.Size(40, 40);
+            this.targetPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.targetPictureBox.TabIndex = 0;
+            this.targetPictureBox.TabStop = false;
+            // 
+            // bulletPictureBox
+            // 
+            this.bulletPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.bulletPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("bulletPictureBox.Image")));
+            this.bulletPictureBox.Location = new System.Drawing.Point(416, 250);
+            this.bulletPictureBox.Name = "bulletPictureBox";
+            this.bulletPictureBox.Size = new System.Drawing.Size(40, 40);
+            this.bulletPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bulletPictureBox.TabIndex = 1;
+            this.bulletPictureBox.TabStop = false;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(24, 359);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start Game";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // fireButton
+            // 
+            this.fireButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("fireButton.BackgroundImage")));
+            this.fireButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.fireButton.Location = new System.Drawing.Point(407, 296);
+            this.fireButton.Name = "fireButton";
+            this.fireButton.Size = new System.Drawing.Size(64, 66);
+            this.fireButton.TabIndex = 3;
+            this.fireButton.UseVisualStyleBackColor = true;
+            this.fireButton.Click += new System.EventHandler(this.fireButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.BackColor = System.Drawing.Color.Transparent;
+            this.statusLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.ForeColor = System.Drawing.Color.Aqua;
+            this.statusLabel.Location = new System.Drawing.Point(391, 365);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(112, 37);
+            this.statusLabel.TabIndex = 4;
+            this.statusLabel.Text = "label1";
+            // 
+            // powPrefixLabel
+            // 
+            this.powPrefixLabel.AutoSize = true;
+            this.powPrefixLabel.BackColor = System.Drawing.Color.Transparent;
+            this.powPrefixLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powPrefixLabel.ForeColor = System.Drawing.Color.Gold;
+            this.powPrefixLabel.Location = new System.Drawing.Point(487, 286);
+            this.powPrefixLabel.Name = "powPrefixLabel";
+            this.powPrefixLabel.Size = new System.Drawing.Size(16, 16);
+            this.powPrefixLabel.TabIndex = 5;
+            this.powPrefixLabel.Text = "0";
+            // 
+            // powSuffixLabel
+            // 
+            this.powSuffixLabel.AutoSize = true;
+            this.powSuffixLabel.BackColor = System.Drawing.Color.Transparent;
+            this.powSuffixLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powSuffixLabel.ForeColor = System.Drawing.Color.Gold;
+            this.powSuffixLabel.Location = new System.Drawing.Point(509, 286);
+            this.powSuffixLabel.Name = "powSuffixLabel";
+            this.powSuffixLabel.Size = new System.Drawing.Size(49, 16);
+            this.powSuffixLabel.TabIndex = 6;
+            this.powSuffixLabel.Text = "Pows!";
+            // 
+            // targetTimer
+            // 
+            this.targetTimer.Tick += new System.EventHandler(this.targetTimer_Tick);
+            // 
+            // bulletTimer
+            // 
+            this.bulletTimer.Tick += new System.EventHandler(this.bulletTimer_Tick);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // powTimer
+            // 
+            this.powTimer.Tick += new System.EventHandler(this.powTimer_Tick);
+            // 
+            // Pow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(564, 405);
+            this.Controls.Add(this.powSuffixLabel);
+            this.Controls.Add(this.powPrefixLabel);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.fireButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.bulletPictureBox);
+            this.Controls.Add(this.targetPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Pow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pow!";
+            this.Load += new System.EventHandler(this.Pow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.targetPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bulletPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox targetPictureBox;
+        private System.Windows.Forms.PictureBox bulletPictureBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button fireButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label powPrefixLabel;
+        private System.Windows.Forms.Label powSuffixLabel;
+        private System.Windows.Forms.Timer targetTimer;
+        private System.Windows.Forms.Timer bulletTimer;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Timer powTimer;
+    }
+}
+
---
+++ A8/Assignment8/Pow.cs
@@ -0,0 +1,167 @@
+﻿//Assignment 8, Ada Ho, CIS345 3:00pm
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Assignment8
+{
+    public partial class Pow : Form
+    {
+        //declaring class variables
+        private Random r;
+        private int targetSpeed;
+        private int totalScore;
+        private int secondsLeft;
+        private SoundPlayer soundPlayer;
+
+        public Pow()
+        {
+            InitializeComponent();
+        }
+
+        // start the game and reset all the properties to an initial state; starts timers
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            r = new Random();
+            targetSpeed = r.Next(4, 15);
+            targetTimer.Interval = 20;
+            gameTimer.Interval = 1000;
+            bulletTimer.Interval = 10;
+            powTimer.Interval = 5;
+            targetPictureBox.Left = 0;
+            bulletPictureBox.Top = 250;
+            totalScore = 0;
+            powPrefixLabel.Text = Convert.ToString(totalScore);
+            statusLabel.Visible = true;
+            targetPictureBox.Visible = true;
+            bulletPictureBox.Visible = true;
+            powSuffixLabel.Visible = true;
+            powPrefixLabel.Visible = true;
+            fireButton.Visible = true;
+            fireButton.Enabled = true;
+            secondsLeft = 60;
+            statusLabel.Text = Convert.ToString(secondsLeft);
+            targetTimer.Start();
+            gameTimer.Start();
+        }
+
+        //moves the target bullet to the right; selects a new random speed when bullet appears on the left
+        private void targetTimer_Tick(object sender, EventArgs e)
+        {
+            targetPictureBox.Left += targetSpeed;
+
+            if (targetPictureBox.Left >= this.Width)
+            {
+                targetPictureBox.Location = new Point(0, 34);
+                targetSpeed = r.Next(4, 15); 
+            }
+        }
+
+        //updates the game timer by changing the label text and updating the secondsLeft variable
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            secondsLeft--;
+
+            if (secondsLeft > 0)
+            {
+                statusLabel.Text = Convert.ToString(secondsLeft);
+            }
+            else
+            {
+                gameTimer.Stop();
+                bulletTimer.Stop();
+                targetTimer.Stop();
+                fireButton.Enabled = false;
+                statusLabel.Text = "Game over";
+            }
+        }
+
+        //fires bullet when the bullet timer starts
+        private void fireButton_Click(object sender, EventArgs e)
+        {
+            bulletPictureBox.Visible = true;
+            bulletTimer.Start();
+        }
+
+        //moves the bullet; calls Collided method to test for collision; if there is, show animation
+        private void bulletTimer_Tick(object sender, EventArgs e)
+        {
+            bulletPictureBox.Top -= 7;
+
+            if (Collided() == true)
+            {
+                totalScore++;
+                powPrefixLabel.Text = Convert.ToString(totalScore);
+                bulletTimer.Stop();
+                targetTimer.Stop();
+                targetPictureBox.ImageLocation = "pow.png"; 
+                targetPictureBox.Height = 0;
+                targetPictureBox.Width = 0;
+                bulletPictureBox.Visible = false;
+                powTimer.Start();
+            }
+
+            if (bulletPictureBox.Top <= 0)
+            {
+                bulletTimer.Stop();
+                bulletPictureBox.Visible = true;
+                bulletPictureBox.Top = 250;
+            }
+        }
+
+        // tests if the target bullet and the fired bullet have collided; returns true if there is overlap, returns false if not
+        private bool Collided()
+        {
+            if((bulletPictureBox.Left >= targetPictureBox.Left && bulletPictureBox.Left <= targetPictureBox.Right)
+                && (bulletPictureBox.Top <= targetPictureBox.Bottom && bulletPictureBox.Top >= targetPictureBox.Top))
+            {
+                return true;
+            }
+
+            if((bulletPictureBox.Right >= targetPictureBox.Left && bulletPictureBox.Right <= targetPictureBox.Right)
+                && (bulletPictureBox.Top < targetPictureBox.Bottom && bulletPictureBox.Top >= targetPictureBox.Top))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //adds a Pow! animation once the bullet and target have collided
+        private void powTimer_Tick(object sender, EventArgs e)
+        {
+            targetPictureBox.Height++;
+            targetPictureBox.Width++;
+
+            if (targetPictureBox.Width == 25)
+            {
+                powTimer.Stop();
+                bulletPictureBox.Top = 250;
+                bulletPictureBox.Visible = true;
+                targetPictureBox.Height = 40;
+                targetPictureBox.Width = 40;
+                targetPictureBox.Location = new Point(0, 34); 
+                targetPictureBox.ImageLocation = "ship.png"; 
+                targetSpeed = r.Next(4, 15); 
+                targetTimer.Start();
+            }
+        }
+        
+        //form load event handler; plays audio file when the app starts and continues looping
+        private void Pow_Load(object sender, EventArgs e)
+        {
+            soundPlayer = new SoundPlayer("s2.wav");
+            soundPlayer.Load();
+            soundPlayer.PlayLooping();
+        }
+    }
+}
