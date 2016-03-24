9c91e503270d89cd66223a582c27a0a3e6f10a77
adaho <adaho@asu.edu>
2016-03-24 12:02:20 -0700

    Added files via upload

---
+++ A7/AverageCalculator/AverageCalculator.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AverageCalculator
+{
+    partial class AverageCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberofScoresLabel = new System.Windows.Forms.Label();
+            this.numberofScoresTextBox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.scoresLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // numberofScoresLabel
+            // 
+            this.numberofScoresLabel.AutoSize = true;
+            this.numberofScoresLabel.Location = new System.Drawing.Point(12, 26);
+            this.numberofScoresLabel.Name = "numberofScoresLabel";
+            this.numberofScoresLabel.Size = new System.Drawing.Size(35, 13);
+            this.numberofScoresLabel.TabIndex = 0;
+            this.numberofScoresLabel.Text = "label1";
+            // 
+            // numberofScoresTextBox
+            // 
+            this.numberofScoresTextBox.Location = new System.Drawing.Point(233, 23);
+            this.numberofScoresTextBox.Name = "numberofScoresTextBox";
+            this.numberofScoresTextBox.Size = new System.Drawing.Size(100, 20);
+            this.numberofScoresTextBox.TabIndex = 1;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(233, 65);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(340, 65);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // scoresLabel
+            // 
+            this.scoresLabel.AutoSize = true;
+            this.scoresLabel.Location = new System.Drawing.Point(12, 99);
+            this.scoresLabel.Name = "scoresLabel";
+            this.scoresLabel.Size = new System.Drawing.Size(35, 13);
+            this.scoresLabel.TabIndex = 4;
+            this.scoresLabel.Text = "label2";
+            // 
+            // AverageCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 111);
+            this.Controls.Add(this.scoresLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.numberofScoresTextBox);
+            this.Controls.Add(this.numberofScoresLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AverageCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Average Calculator";
+            this.Load += new System.EventHandler(this.AverageCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numberofScoresLabel;
+        private System.Windows.Forms.TextBox numberofScoresTextBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label scoresLabel;
+    }
+}
+
---
+++ A7/AverageCalculator/AverageCalculator.cs
@@ -0,0 +1,74 @@
+﻿//Assignment 7, Ada Ho, CIS345 3:00pm
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageCalculator
+{
+    public partial class AverageCalculator : Form
+    {
+        //declare private class variables
+        private TextBox[] scoreTextBoxArray;
+
+        public AverageCalculator()
+        {
+            InitializeComponent();
+        }
+
+        //load event handler for avg calculator form
+        private void AverageCalculator_Load(object sender, EventArgs e)
+        {
+            calculateButton.Visible = false;
+            numberofScoresLabel.Text = "How many scores would you like to enter?";
+            scoresLabel.Text = "";
+        }
+
+        //click event handler for the OK button
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            int numberOfTextBoxes;
+            numberOfTextBoxes = Convert.ToInt32(numberofScoresTextBox.Text);
+            this.Height = 500;
+            calculateButton.Visible = true;
+            okButton.Enabled = false;
+            CreateTextBoxes(numberOfTextBoxes);
+        }
+
+        //instantiating textbox array, assigning user input into array, and formatting spacing around each textbox
+        private void CreateTextBoxes(int number)
+        {
+            scoreTextBoxArray = new TextBox[number];
+            int verticalTopPosition = 150;
+
+            for (int i = 0; i < scoreTextBoxArray.Length; i++)
+            {
+                scoreTextBoxArray[i] = new TextBox();
+                scoreTextBoxArray[i].Left = 20;
+                scoreTextBoxArray[i].Top = verticalTopPosition;
+                verticalTopPosition += 50;
+                this.Controls.Add(scoreTextBoxArray[i]);
+            }
+        }
+
+        //Calculate button click event handler
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double total = 0.0;
+            double average = 0.0;
+
+            for (int i = 0; i < scoreTextBoxArray.Length; i++)
+            {
+                total += Convert.ToInt32(scoreTextBoxArray[i].Text);
+            }
+
+            average = total / scoreTextBoxArray.Length;
+            scoresLabel.Text = "The average of the following scores is " + average;
+        }
+    }
+}
