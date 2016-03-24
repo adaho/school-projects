616029d004d1c76ec96df00a84951977f7a82411
adaho <adaho@asu.edu>
2016-03-24 12:01:48 -0700

    Added files via upload

---
+++ A6/Assignment6/Book.cs
@@ -0,0 +1,53 @@
+﻿//Assignment 6, Ada Ho, CIS345 3:00pm
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    class Book
+    {
+        //declaring class variables
+        public string title;
+        public int year;
+
+        //Title property
+        private string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        //Year property
+        private int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        //Book constructor
+        public Book()
+        {
+            Console.WriteLine("A new book has been added to the library.");
+        }
+
+        //overloaded Book constructor
+        public Book(string Title, int Year)
+        {
+            this.title = Title;
+
+            if (Year < 2015 && Year > 1100)
+            {
+                this.year = Year;
+            }
+            else
+            {
+                year = 1900;
+            }
+
+            Console.WriteLine("Title '{0}' added to the library.", this.title);
+        }
+    }
+}
---
+++ A6/Assignment6/LibrarySystem.cs
@@ -0,0 +1,90 @@
+﻿//Assignment 6, Ada Ho, CIS345 3:00pm
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    class LibrarySystem
+    {
+        //declaring private class variables
+        private Book[] newLibrary;
+        private string bookTitle;
+        private int bookYear;
+        private int bookCount;
+
+        static void Main(string[] args)
+        {
+            LibrarySystem myLibrary = new LibrarySystem();
+            myLibrary.LoadLibrarySystem();
+            Console.ReadLine();
+        }
+
+        //default constructor
+        public LibrarySystem()
+        {
+            
+        }
+
+        public void DisplayHeader()
+        {
+            Console.Clear();
+            string title = "New Library System";
+            Console.WriteLine("{0," + WriteCenteredLine(title) + "}\n\n", title);
+        }
+
+        //method for centering the Header
+        private static int WriteCenteredLine(string text)
+        {
+            int x;
+            x = (Console.WindowWidth + text.Length) / 2;
+            return x;
+        }
+
+        //getting info from user and using that info to add a book to the newLibrary array
+        public void AddBook()
+        {
+            Console.Write("\nEnter Book title: ");
+            bookTitle = Console.ReadLine();
+            Console.Write("Enter Book year: ");
+            bookYear = Convert.ToInt32(Console.ReadLine());
+
+            Book newBook = new Book(bookTitle, bookYear);
+            newLibrary[bookCount] = newBook;
+            bookCount++;
+        }
+
+        //going through the newLibrary array using a loop and displaying each new book's title and year
+        public void DisplayBookList()
+        {
+            Console.WriteLine("{0,-50}{1}", "Title", "Year\n");
+
+            for (int i = 0; i < bookCount; i++)
+            {
+                Console.WriteLine("{0,-50}{1}", newLibrary[i].title, newLibrary[i].year);
+            }
+        }
+
+        //new library transactions
+        public void LoadLibrarySystem()
+        {
+            Console.Write("How many new books do you want to add to the Library? ");
+            int bookSize = Convert.ToInt32(Console.ReadLine());
+            newLibrary = new Book[bookSize];
+
+            for (int i = 0; i < bookSize; i++)
+            {
+                AddBook();
+            }
+            
+            Console.WriteLine("\n\nAdding books complete. Press enter to continue.");
+            Console.ReadLine();
+
+            DisplayHeader();
+            DisplayBookList();
+        }
+   
+    }
+}
